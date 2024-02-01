36c0738598f17cdd8c2ec9908d61aefae7c61182
bryllim <bryllim@gmail.com>
2024-02-01 20:26:09 +0800

    first commit

---
+++ app/Http/Controllers/TodoController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\Todo;
+
+class TodoController extends Controller
+{
+    public function create(){
+        $todo = new Todo();
+        $todo->content = "Test content";
+        $todo->status = "pending";
+        $todo->save();
+
+        return "Added a new todo!";
+    }
+}
---
+++ resources/views/todo.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+
+<body>
+    <main class="container m-5 p-5 border rounded">
+        <h1 class="fw-bold">To-do List</h1>
+        <hr>
+        <form action="#">
+            <input type="text" class="form-control">
+            <button class="btn btn-primary btn-sm mt-3">Add</button>
+        </form>
+        <hr>
+        <div class="form-check">
+            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+            <label class="form-check-label" for="flexCheckDefault">
+                Default checkbox
+            </label>
+        </div>
+        <div class="form-check">
+            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+            <label class="form-check-label" for="flexCheckDefault">
+                Default checkbox
+            </label>
+        </div>
+        <div class="form-check">
+            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+            <label class="form-check-label" for="flexCheckDefault">
+                Default checkbox
+            </label>
+        </div>
+    </main>
+</body>
+
+</html>
